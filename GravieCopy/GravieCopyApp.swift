9090e58b1937f0c4dd030851058a990c1a5014f6
Thales Nunes <thalesgsn@gmail.com>
2026-05-23 00:00:55 -0300

    Initial Commit

---
+++ GravieCopy/GravieCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GravieCopyApp.swift
+//  GravieCopy
+//
+//  Created by Thales Nunes on 23/05/26.
+//
+
+import SwiftUI
+
+@main
+struct GravieCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
